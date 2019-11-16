bde4894c011a3f0757ddafb97e39efe107a41ef7
TakuyaTaniguchi <ttaniguchi@oikaze.jp>
2019-11-16 22:25:14 +0900

    Update createStyle

--- content.js
+++ content.js
@@ -1,3 +1,16 @@
-console.log('contents');
 const body = document.querySelector('body');
-console.log(body)
+const templateStyletag = document.createElement('style');
+
+const css = `
+* { background-color: rgba(255,0,0,.2); }
+* * { background-color: rgba(0,255,0,.2); }
+* * * { background-color: rgba(0,0,255,.2); }
+* * * * { background-color: rgba(255,0,255,.2); }
+* * * * * { background-color: rgba(0,255,255,.2); }
+* * * * * * { background-color: rgba(255,255,0,.2); }
+* * * * * * * { background-color: rgba(255,0,0,.2); }
+* * * * * * * * { background-color: rgba(0,255,0,.2); }
+* * * * * * * * * { background-color: rgba(0,0,255,.2); }
+`
+templateStyletag.appendChild(document.createTextNode(css));
+console.log(templateStyletag)
