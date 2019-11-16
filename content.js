8ce05186baa74d99dad17b0428f4e5c70f3858be
TakuyaTaniguchi <ttaniguchi@oikaze.jp>
2019-11-17 00:42:15 +0900

    Update active機能の追加

--- content.js
+++ content.js
@@ -1,16 +1,29 @@
-const body = document.querySelector('body');
-const templateStyletag = document.createElement('style');
+chrome.runtime.onMessage.addListener(
+    function(request, sender, sendResponse) {
+      if( request.message === "active" ) {
+        active();
+        }
+    }
+  );
 
-const css = `
-* { background-color: rgba(255,0,0,.2); }
-* * { background-color: rgba(0,255,0,.2); }
-* * * { background-color: rgba(0,0,255,.2); }
-* * * * { background-color: rgba(255,0,255,.2); }
-* * * * * { background-color: rgba(0,255,255,.2); }
-* * * * * * { background-color: rgba(255,255,0,.2); }
-* * * * * * * { background-color: rgba(255,0,0,.2); }
-* * * * * * * * { background-color: rgba(0,255,0,.2); }
-* * * * * * * * * { background-color: rgba(0,0,255,.2); }
-`
-templateStyletag.appendChild(document.createTextNode(css));
-console.log(templateStyletag)
+function active(){
+    const head = document.getElementsByTagName('head').item(0);
+    const style = document.createElement('style');
+    const text = `
+        * { background-color: rgba(255,0,0,.2); }
+        * * { background-color: rgba(0,255,0,.2); }
+        * * * { background-color: rgba(0,0,255,.2); }
+        * * * * { background-color: rgba(255,0,255,.2); }
+        * * * * * { background-color: rgba(0,255,255,.2); }
+        * * * * * * { background-color: rgba(255,255,0,.2); }
+        * * * * * * * { background-color: rgba(255,0,0,.2); }
+        * * * * * * * * { background-color: rgba(0,255,0,.2); }
+        * * * * * * * * * { background-color: rgba(0,0,255,.2); }
+    `;
+    const rule = document.createTextNode(text);
+    style.media = 'screen';
+    style.type = 'text/css';
+    style.appendChild(rule);
+    head.appendChild(style);
+}
+
--- popup.js
+++ popup.js
@@ -1,21 +1,14 @@
 'use strict';
 
-const $textarea = document.getElementById('textarea');
-const $button = document.getElementById('button');
-const $result = document.getElementById('result');
 
 function onClickButton() {
-  const items = $textarea.value.split(' ');
-  $result.innerHTML = shuffle(items).join(' ');
+    chrome.tabs.query({currentWindow: true, active: true}, function (tabs){
+        var activeTab = tabs[0];
+        chrome.tabs.sendMessage(activeTab.id, {"message": "active"});
+     });
 }
 
-function shuffle(items) {
-  const array = items.slice();
-  for (let i = array.length - 1; i > 0; i--) {
-    let r = Math.floor(Math.random() * (i + 1));
-    [array[i], array[r]] = [array[r], array[i]];
-  }
-  return array;
-}
 
-$button.addEventListener('click', onClickButton);
+document.addEventListener("DOMContentLoaded", function() {
+  document.getElementById("button").addEventListener("click", onClickButton);
+});
